1bef5823b2bdb38170013d3db259c2e88bfbddf8
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-02-23 11:45:10 +0100

    feat: add group

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const plane = new THREE.Mesh(
     side: THREE.DoubleSide,
   })
 )
-scene.add(plane)
+plane.position.z = -0.001
 
 // ____ SHAPES ____
 const cube1 = new THREE.Mesh(
@@ -31,69 +31,65 @@ const cube1 = new THREE.Mesh(
   new THREE.MeshBasicMaterial({ color: 0xff0000 })
 )
 cube1.position.set(-5, -5, 1)
-scene.add(cube1)
 const cube2 = new THREE.Mesh(
   new THREE.BoxGeometry(2, 2, 2),
   new THREE.MeshBasicMaterial({ color: 0x00ff00 })
 )
 cube2.position.set(-5, 0, 1)
-scene.add(cube2)
 const cube3 = new THREE.Mesh(
   new THREE.BoxGeometry(2, 2, 2),
   new THREE.MeshBasicMaterial({ color: 0x0000ff })
 )
 cube3.position.set(-5, 5, 1)
-scene.add(cube3)
 const cube4 = new THREE.Mesh(
   new THREE.BoxGeometry(2, 2, 2),
   new THREE.MeshBasicMaterial({ color: 0xffff00 })
 )
 cube4.position.set(0, -5, 1)
-scene.add(cube4)
 const cube5 = new THREE.Mesh(
   new THREE.BoxGeometry(2, 2, 2),
   new THREE.MeshBasicMaterial({ color: 0x00ffff })
 )
 cube5.position.set(0, 0, 1)
-scene.add(cube5)
 const cube6 = new THREE.Mesh(
   new THREE.BoxGeometry(2, 2, 2),
   new THREE.MeshBasicMaterial({ color: 0xffffff })
 )
 cube6.position.set(0, 5, 1)
-scene.add(cube6)
 const cube7 = new THREE.Mesh(
   new THREE.BoxGeometry(2, 2, 2),
   new THREE.MeshBasicMaterial({ color: 0xff00ff })
 )
 cube7.position.set(5, -5, 1)
-scene.add(cube7)
 const cube8 = new THREE.Mesh(
   new THREE.BoxGeometry(2, 2, 2),
   new THREE.MeshBasicMaterial({ color: 0x99ffdd })
 )
 cube8.position.set(5, 0, 1)
-scene.add(cube8)
 const cube9 = new THREE.Mesh(
   new THREE.BoxGeometry(2, 2, 2),
   new THREE.MeshBasicMaterial({ color: 0x999999 })
 )
 cube9.position.set(5, 5, 1)
-scene.add(cube9)
 
+const group = new THREE.Group()
+group.add(plane, cube1, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9)
+scene.add(group)
 
 // ____ ANIMATIONS ____
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
+  group.rotation.y += 0.01
+  group.rotation.x += 0.01
   cube1.rotation.z += 0.01
-  cube2.rotation.z += 0.01
+  cube2.rotation.z -= 0.01
   cube3.rotation.z += 0.01
-  cube4.rotation.z += 0.01
+  cube4.rotation.z -= 0.01
   cube5.rotation.z += 0.01
-  cube6.rotation.z += 0.01
+  cube6.rotation.z -= 0.01
   cube7.rotation.z += 0.01
-  cube8.rotation.z += 0.01
+  cube8.rotation.z -= 0.01
   cube9.rotation.z += 0.01
 }
 
